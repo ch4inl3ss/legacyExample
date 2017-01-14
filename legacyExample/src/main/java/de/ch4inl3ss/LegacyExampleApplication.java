bde2224f5853a5d3d0244ed69464fd4cbccd5fb0
ch4inl3ss <ch4inl3ss@gmail.com>
2017-01-14 19:55:28 +0100

    Spring Boot

---
+++ legacyExample/src/main/java/de/ch4inl3ss/LegacyExampleApplication.java
@@ -0,0 +1,12 @@
+package de.ch4inl3ss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegacyExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegacyExampleApplication.class, args);
+	}
+}
---
+++ legacyExample/src/test/java/de/ch4inl3ss/LegacyExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package de.ch4inl3ss;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LegacyExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
