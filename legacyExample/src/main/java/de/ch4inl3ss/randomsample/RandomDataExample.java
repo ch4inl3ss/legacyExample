7d2b60c18da7f4476f6c1d388ab973e1f051c04c
ch4inl3ss <ch4inl3ss@gmail.com>
2018-03-11 18:49:05 +0100

    vortrag fertig

---
+++ legacyExample/src/main/java/de/ch4inl3ss/randomsample/RandomDataExample.java
@@ -0,0 +1,13 @@
+package de.ch4inl3ss.randomsample;
+
+public class RandomDataExample {
+
+    public String someLogicHere(int parameter){
+        if(parameter==10){
+            return "10";
+        }else{
+            return "nicht 10";
+        }
+    }
+
+}
